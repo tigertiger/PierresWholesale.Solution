8afd77ecf373fbdb574a7445fc3fb946de50f847
Melissa <melissa.scmi@gmail.com>
2021-10-01 09:03:32 -0700

    pass T1: create instance of vendor

--- PierresWholesale.Tests/ModelTests/VendorTests.cs
+++ PierresWholesale.Tests/ModelTests/VendorTests.cs
@@ -8,6 +8,11 @@ namespace PierresWholesale.Tests
   [TestClass]
   public class VendorTests
   {
-    
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("Sarah's Bad Bakery", "Sells Half-Buns and Orts");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
   }
 }
--- PierresWholesale/Models/Vendor.cs
+++ PierresWholesale/Models/Vendor.cs
@@ -3,6 +3,14 @@ namespace PierresWholesale.Models
 {
   public class Vendor
   {
+    public string Name { get; set; }
+    public string Details { get; set; }
+    public int Id { get ;}
 
+    public Vendor (string name, string details)
+    {
+      Name = name;
+      Details = details;
+    }
   }
 }
