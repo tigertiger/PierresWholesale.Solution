c92c84e3ebccce773b32b6e5e2b1606923c61eac
Melissa <melissa.scmi@gmail.com>
2021-10-01 10:16:45 -0700

    pass T9: return order properties

--- PierresWholesale.Tests/ModelTests/OrderTests.cs
+++ PierresWholesale.Tests/ModelTests/OrderTests.cs
@@ -15,5 +15,26 @@ namespace PierresWholesale.Tests
       Order newOrder = new Order("Sunday Bunday", "300 half-buns", 450, "10/1/2021");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+
+    //9
+    [TestMethod]
+    public void GetOrderProperties_ReturnsOrderProperties_StringStringIntString()
+    {
+      string orderName = "Sunday Bunday";
+      string orderDetails = "300 half-buns";
+      int orderPrice = 450;
+      string orderDate = "10/1/2021";
+
+      Order newOrder = new Order(orderName, orderDetails, orderPrice, orderDate);
+      string nameResult = newOrder.OrderName;
+      string detailsResult = newOrder.OrderDetails;
+      int priceResult = newOrder.OrderPrice;
+      string dateResult = newOrder.OrderDate;
+
+      Assert.AreEqual(orderName, nameResult);
+      Assert.AreEqual(orderDetails, detailsResult);
+      Assert.AreEqual(orderPrice, priceResult);
+      Assert.AreEqual(orderDate, dateResult);
+    }
   }
 }
