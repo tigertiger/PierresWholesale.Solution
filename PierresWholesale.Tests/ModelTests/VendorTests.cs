f8462fa84b6a92465a5615d7476152d37f99ab06
Melissa <melissa.scmi@gmail.com>
2021-10-01 08:35:25 -0700

    add initial file structure and framework

---
+++ PierresWholesale.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using PierresWholesale.Models;
+using System;
+
+namespace PierresWholesale.Tests
+{
+  [TestClass]
+  public class VendorTests
+  {
+    
+  }
+}
---
+++ PierresWholesale/Models/Vendor.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+namespace PierresWholesale.Models
+{
+  public class Vendor
+  {
+
+  }
+}
